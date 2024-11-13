330d69c241e36da9685977cdd95e894393154fa6
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2024-11-13 09:56:17 +0300

    my first succesfull project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <h1 className='text-red-500'>hello world</h1>
+  )
+}
+
+export default App
